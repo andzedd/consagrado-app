81e610abbe5ea65cbae9e1627cf7036a07b09978
Matheus S Souza <andzedd.matheus@gmail.com>
2020-11-27 20:26:05 -0300

    added logo

--- app/src/main/java/com/consagrado/consagradoapp/Activities/MainActivity.java
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.consagrado.consagradoapp.R;
@@ -14,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        (new Handler()).postDelayed(this::openInitial, 5000);
     }
 
-    public void openInitial(View view){
+    public void openInitial(){
         Intent it = new Intent(this, InitialActivity.class);
         startActivity(it);
+        finish();
 //        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 }
