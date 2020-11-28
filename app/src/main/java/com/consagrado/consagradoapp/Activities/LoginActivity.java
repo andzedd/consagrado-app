654e97f6ae26ed94ca9c6bf3242ee9a5a4e20367
Matheus S Souza <andzedd.matheus@gmail.com>
2020-11-27 23:45:41 -0300

    options screen

--- app/src/main/java/com/consagrado/consagradoapp/Activities/LoginActivity.java
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/LoginActivity.java
@@ -2,7 +2,9 @@ package com.consagrado.consagradoapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.consagrado.consagradoapp.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void openOptions(View view){
+        Intent it = new Intent(this, OptionsActivity.class);
+        startActivity(it);
     }
 }
---
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/OptionsActivity.java
@@ -0,0 +1,16 @@
+package com.consagrado.consagradoapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.consagrado.consagradoapp.R;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
