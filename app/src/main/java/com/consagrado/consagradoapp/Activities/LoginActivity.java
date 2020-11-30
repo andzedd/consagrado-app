f3fd1657c0bed7c3aa6c514227bb9212348be9fd
Matheus S Souza <andzedd.matheus@gmail.com>
2020-11-30 12:48:36 -0300

    password toggle visibility

--- app/src/main/java/com/consagrado/consagradoapp/Activities/LoginActivity.java
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/LoginActivity.java
@@ -4,20 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.consagrado.consagradoapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText senha;
+    private ImageView visibilidadeSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        iniciaComponentes();
     }
 
     public void openOptions(View view){
         Intent it = new Intent(this, OptionsActivity.class);
         startActivity(it);
+    }
+
+    public void iniciaComponentes(){
+        senha               = findViewById(R.id.inputLoginSenha);
+        visibilidadeSenha   = findViewById(R.id.imgVisivel);
+
+        visibilidadeSenha.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.imgVisivel:
+                if(visibilidadeSenha.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.ic_visibility).getConstantState())){
+                    senha.setTransformationMethod(null);
+                    visibilidadeSenha.setImageResource(R.drawable.ic_visibility_off);
+                } else {
+                    visibilidadeSenha.setImageResource(R.drawable.ic_visibility);
+                    senha.setTransformationMethod(new PasswordTransformationMethod());
+
+                }
+                senha.setSelection(senha.getText().length());
+                break;
+        }
     }
 }
--- app/src/main/java/com/consagrado/consagradoapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/RegisterActivity.java
@@ -3,14 +3,58 @@ package com.consagrado.consagradoapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.consagrado.consagradoapp.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText txtSenha, txtSenhaR;
+    private ImageView visibilidadeSenha, visibilidadeSenhaR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        iniciaComponentes();
+    }
+
+    public void iniciaComponentes(){
+        visibilidadeSenha = findViewById(R.id.imgVisivelR);
+        visibilidadeSenhaR = findViewById(R.id.imgVisivelRC);
+        txtSenha = findViewById(R.id.inputSenha);
+        txtSenhaR = findViewById(R.id.inputRSenha);
+
+        visibilidadeSenha.setOnClickListener(this);
+        visibilidadeSenhaR.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imgVisivelR:
+                if(visibilidadeSenha.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.ic_visibility).getConstantState())){
+                    txtSenha.setTransformationMethod(null);
+                    visibilidadeSenha.setImageResource(R.drawable.ic_visibility_off);
+                } else {
+                    visibilidadeSenha.setImageResource(R.drawable.ic_visibility);
+                    txtSenha.setTransformationMethod(new PasswordTransformationMethod());
+                }
+                txtSenha.setSelection(txtSenha.getText().length());
+                break;
+            case R.id.imgVisivelRC:
+                if(visibilidadeSenhaR.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.ic_visibility).getConstantState())){
+                    txtSenhaR.setTransformationMethod(null);
+                    visibilidadeSenhaR.setImageResource(R.drawable.ic_visibility_off);
+                } else {
+                    visibilidadeSenhaR.setImageResource(R.drawable.ic_visibility);
+                    txtSenhaR.setTransformationMethod(new PasswordTransformationMethod());
+                }
+                txtSenhaR.setSelection(txtSenhaR.getText().length());
+                break;
+
+        }
     }
 }
