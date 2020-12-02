16de0d1222edfc8db6f523374b117bdbdf0f6fdb
Matheus S Souza <andzedd.matheus@gmail.com>
2020-12-02 20:39:19 -0300

    login

--- app/src/main/java/com/consagrado/consagradoapp/Activities/InitialActivity.java
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/InitialActivity.java
@@ -8,24 +8,46 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
+import android.widget.Button;
 
 import com.consagrado.consagradoapp.R;
 
-public class InitialActivity extends AppCompatActivity {
+public class InitialActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+        iniciaComponentes();
     }
 
-    public void openRegister(View view){
+    private void iniciaComponentes(){
+        btnLogin = findViewById(R.id.btnInitialLogin);
+        btnRegister = findViewById(R.id.btnInitialRegister);
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+    }
+
+    private void openRegister(){
         Intent it = new Intent(this, RegisterActivity.class);
         startActivity(it);
     }
 
-    public void openLogin(View view){
+    private void openLogin(){
         Intent it = new Intent(this, LoginActivity.class);
         startActivity(it);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnInitialLogin:
+                openLogin();
+                break;
+            case R.id.btnInitialRegister:
+                openRegister();
+                break;
+        }
     }
 }
--- app/src/main/java/com/consagrado/consagradoapp/Activities/LoginActivity.java
+++ app/src/main/java/com/consagrado/consagradoapp/Activities/LoginActivity.java
@@ -1,19 +1,29 @@
 package com.consagrado.consagradoapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.consagrado.consagradoapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText senha;
+    private EditText editTextEmail, editTextSenha;
     private ImageView visibilidadeSenha;
+    private FirebaseAuth mAuth;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +32,63 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         iniciaComponentes();
     }
 
-    public void openOptions(View view){
-        Intent it = new Intent(this, OptionsActivity.class);
-        startActivity(it);
+    private void userLogin(){
+        if(validaCampos()){
+            String email = editTextEmail.getText().toString().trim();
+            String senha = editTextSenha.getText().toString().trim();
+            mAuth.signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        startActivity(new Intent(getApplicationContext(), OptionsActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Usuário não encontrado", Toast.LENGTH_LONG).show();
+                        editTextSenha.setText("");
+                        editTextEmail.requestFocus();
+                    }
+                }
+            });
+        }
     }
 
-    public void iniciaComponentes(){
-        senha               = findViewById(R.id.inputLoginSenha);
+    private boolean validaCampos(){
+        String email = editTextEmail.getText().toString().trim();
+        String senha = editTextSenha.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Por favor preencha seu email!");
+            editTextEmail.requestFocus();
+            return false;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Por favor insira um email válido!");
+            editTextEmail.requestFocus();
+            return false;
+        }
+        if(senha.isEmpty()){
+            editTextSenha.setError("Por favor preencha sua senha!");
+            editTextSenha.requestFocus();
+            return false;
+        }
+        if(senha.length() < 6){
+            editTextSenha.setError("A senha precisa ter ao menos 6 caracteres!");
+            editTextSenha.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+    private void iniciaComponentes(){
+        editTextEmail       = findViewById(R.id.inputLoginEmail);
+        editTextSenha       = findViewById(R.id.inputLoginSenha);
         visibilidadeSenha   = findViewById(R.id.imgVisivel);
+        btnLogin            = findViewById(R.id.btnLogin);
 
         visibilidadeSenha.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -39,15 +96,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
             case R.id.imgVisivel:
                 if(visibilidadeSenha.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.ic_visibility).getConstantState())){
-                    senha.setTransformationMethod(null);
+                    editTextSenha.setTransformationMethod(null);
                     visibilidadeSenha.setImageResource(R.drawable.ic_visibility_off);
                 } else {
                     visibilidadeSenha.setImageResource(R.drawable.ic_visibility);
-                    senha.setTransformationMethod(new PasswordTransformationMethod());
+                    editTextSenha.setTransformationMethod(new PasswordTransformationMethod());
 
                 }
-                senha.setSelection(senha.getText().length());
+                editTextSenha.setSelection(editTextSenha.getText().length());
                 break;
+            case R.id.btnLogin:
+                userLogin();
         }
     }
 }
